75ce0fb7b0ae0a7239b6204a60543e88ca1be28e
pendulum <rs.pendulum@gmail.com>
2024-12-03 19:12:51 +0300

    made example logic and setup jest

---
+++ test/main.test.ts
@@ -0,0 +1,8 @@
+import { exampleFunction } from "src";
+
+describe('package name', function () {
+  it('example', () => {
+    const result = exampleFunction();
+    expect(result).toEqual("Hello World!");
+  });
+});
